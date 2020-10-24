d7f31fd3be44e3de309e6829050f3f8ab05c8767
unknown <[EMAIL]>
2020-10-24 20:27:12 +0200

    descontrolado

--- scripts1.js
+++ scripts1.js
@@ -130,7 +130,9 @@ pintar3(mipregunta[0]);
 
 function pintar4(respuestaPintar3){
 	let variablerespuesta4 = document.createElement("h4");
+
 	document.getElementsByClassName("caja5")[0].appendChild(variablerespuesta4);
+
 	variablerespuesta4.innerText = respuestaPintar3.respuestas[3];
 }
 pintar4(mipregunta[0]);
@@ -139,9 +141,43 @@ pintar4(mipregunta[0]);
 
 let a = document.querySelector("#submit");
 
+let otrarespuesta = document.querySelectorAll("input");
+
+
+/*
 function prevenir(event) {
-    event.preventDefault();
+	
+event.preventDefault();
+	if ( y.value === "mago" ){
+		console.log("acierto")
+	}
+	else { 
+		console.log("error");
+	}
 }
 a.addEventListener("click", prevenir );
+ */
+
+
+
+
+
+
+//console.log(mipregunta[0].respuestacorrecta === "c")
 
 // evita el refresco de submit
+// y al mismo tiempo mira la respuesta correcta
+/*
+const mipregunta = [
+	{
+		pregunta: "En el mundo de Dragones y Mazmorras, ¿Quién es Elminster?",
+		respuestas: [
+			 "Un bardo",
+			 "Un mercader",
+             "Un mago",
+             "Un marinero"
+		],
+		respuestacorrecta: 'c'
+	},
+];
+*/
